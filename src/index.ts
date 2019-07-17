c5d8d5f62640090dc12fccd36a2a10724184a4cc
Sean Wilson <seancwilson@gmail.com>
2019-07-17 17:08:09 -0500

    Add plugin & decorator

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { createDecorator } from 'vue-class-component'
+import AsyncComputedPlugin, { AsyncComputedOption, AsyncComputedPluginOptions } from 'vue-async-computed'
+import Vue, { PluginObject } from 'vue'
+
+export type AsyncComputedOptions<T> = {
+	default?: (() => T) | T,
+	watch?: string[],
+	shouldUpdate?(): boolean
+	lazy?: boolean
+}
+
+type AsyncComputedDecorator = (<T>(options?: AsyncComputedOptions<T>) => MethodDecorator) & PluginObject<AsyncComputedPluginOptions>
+
+const AsyncComputed = function <T = any>(options?: AsyncComputedOptions<T>) {
+	return (target: Vue, propertyKey: string , descriptor: TypedPropertyDescriptor<T>) => {
+		createDecorator((vueOpts, prop: string) => {
+			const { asyncComputed = {}, computed: { [prop]: opt, ...computed } = {}} = vueOpts
+			asyncComputed[prop] = {
+				get: (opt instanceof Function) ? opt : opt.get,
+				...options
+			} as AsyncComputedOption
+			vueOpts.computed = computed
+			vueOpts.asyncComputed = asyncComputed
+		})(target, propertyKey)
+	}
+} as AsyncComputedDecorator
+
+AsyncComputed.install = AsyncComputedPlugin.install
+
+export default AsyncComputed
