39d400efb6e6e057c30dde4b12ffeb78b9718320
Sean Wilson <seancwilson@gmail.com>
2020-08-19 16:46:06 -0500

    remove embedded `vue-async-computed` re-export

--- packages/vue-async-computed/README.md
+++
@@ -1,1 +0,0 @@
-Re-export of the [`vue-async-computed`](https://github.com/foxbenjaminfox/vue-async-computed) package with TypeScript definitions.
--- packages/vue-async-computed/index.d.ts
+++
@@ -1,4 +0,0 @@
-import './vue';
-import './vue-async-computed';
-export * from 'vue-async-computed';
-export { default as default } from 'vue-async-computed';
